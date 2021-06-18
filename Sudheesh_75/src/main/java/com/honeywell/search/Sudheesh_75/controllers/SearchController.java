0fe0fe10048f5665f1ce810e5c8f19dfe3f9429b
Sudheesh G S <gs.sudheesh@gmail.com>
2021-06-18 10:49:58 +0530

    Added controller service link

--- Sudheesh_75/src/main/java/com/honeywell/search/Sudheesh_75/controllers/SearchController.java
+++ Sudheesh_75/src/main/java/com/honeywell/search/Sudheesh_75/controllers/SearchController.java
@@ -8,11 +8,13 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.honeywell.search.Sudheesh_75.dtos.Event;
+import com.honeywell.search.Sudheesh_75.services.SearchService;
 
 /**
  * @author Sudheesh G S
@@ -24,13 +26,25 @@ public class SearchController {
 	
 	private static final Logger LOGGER = LogManager.getLogger(SearchController.class);
 
+	@Autowired
+	private SearchService searchService;
+	
 	@SuppressWarnings("unchecked")
-	@GetMapping("/search")
+	@GetMapping("/searchRandom")
 	public List<Event> findEvents(@RequestParam(value = "key") String searchKey){
 		if(searchKey.isBlank()) {
 			LOGGER.error("Expected search value, found empty. Returning empty result");
 			return Collections.EMPTY_LIST;
 		}
-		return null;
+		return searchService.getEvents(searchKey);
+	}
+	
+	@GetMapping("/searchRegistered")
+	public List<Event> findRegisteredEvents(@RequestParam(value = "key") String searchKey){
+		if(searchKey.isBlank()) {
+			LOGGER.error("Expected search value, found empty. Returning empty result");
+			return Collections.EMPTY_LIST;
+		}
+		return searchService.getEventsRegistered(searchKey);
 	}
 }
